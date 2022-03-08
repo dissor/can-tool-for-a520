3b184e947b61e762a8f7e7bb66906bc3812eac4d
guojianwen <guojianwenjonas@foxmail.com>
2022-03-08 15:19:14 +0800

    尝试将升级独立线程，添加升级超时退出

--- DB/test.py
+++ DB/test.py
@@ -1,20 +1,3 @@
-import sqlite3
-
-# 创建与数据库的连接
-conn = sqlite3.connect(':memory:')
-
-# 创建一个游标
-cur = conn.cursor()
-
-# 建表的sql语句
-sql_test_1 = '''CREATE TABLE scores (
-    SN TEXT
-)'''
-
-# 执行sql语句
-cur.execute(sql_test_1)
-
-print("tishiiiii")
 
 SN1, SN2 = "", ""
 STATE_MN, STATE_CPUN, STATE_VMN, STATE_PN = "", "", "", ""
--- main.py
+++ main.py
@@ -42,45 +42,12 @@ class recv_worker(QtCore.QThread):
         #       "len: ", len(recv_data1.arryData))
         widget.recv_data_loop(recv_data1)
 
-
-class MyWidget(QtWidgets.QWidget):
+# 升级线程类
+class update_worker(QtCore.QThread):
     def __init__(self):
         super().__init__()
 
-        self.ui = ui_00login.Ui_Form()  # 实例化UI对象
-        self.ui.setupUi(self)  # 初始化
-
-        self.init_data()
-
-    # 测试接口
-    def ceshi_api(self):
-        print(sys._getframe().f_code.co_name)
-        # self.ui.lb_sn.setText(recv_data.arryData[0])
-
-    # 打开升级文件
-    def openBinFile(self):
-        print(sys._getframe().f_code.co_name)
-        upgrade.FILE_NAME = ""
-        # 参数 _ 用于分割第二个返回值，防止 upgrade.FILE_NAME 生成数组
-        upgrade.FILE_NAME, _ = QtWidgets.QFileDialog.getOpenFileName(
-            self, "选择文件", "", "BIN(*.bin);;All Files(*)")
-        self.ui.lb_binpath.setText(upgrade.FILE_NAME)
-
-        upgrade.FILE_SZ = os.path.getsize(upgrade.FILE_NAME)
-        print(upgrade.FILE_SZ)
-        self.ui.lb_binsize.setText("%d" % upgrade.FILE_SZ+" bytes")
-
-        # 使用open函数打开文件，打开模式选择二进制读取'rb'
-        upgrade.FILE_FD = open(upgrade.FILE_NAME, 'rb')
-
-        # upgrade.FILE_FD.seek(0)
-        # print(upgrade.FILE_FD.read(4))
-        # for i in upgrade.FILE_FD:
-        #     print(i)
-        # upgrade.FILE_FD.close()
-
-    # 开始升级
-    def startUpgrade(self):
+    def run(self):
         global update_event, update_queue_start_cb, update_queue_send_cb
         print(sys._getframe().f_code.co_name)
         global devHandle
@@ -161,13 +128,146 @@ class MyWidget(QtWidgets.QWidget):
                 data_res = update_queue_send_cb.get()
                 if data_res[0] == 0:
                     print("接收成功: ",index/upgrade.FILE_CNT*100)
-                    self.ui.progressBar.setValue(index/upgrade.FILE_CNT*100)
+                    widget.ui.progressBar.setValue(index/upgrade.FILE_CNT*100)
                 else:
                     print("接收失败")
                     for i in data_res:
                         print(hex(i), end='\t')
             else:
                 print("升级包发送失败")
+
+
+
+class MyWidget(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.ui = ui_00login.Ui_Form()  # 实例化UI对象
+        self.ui.setupUi(self)  # 初始化
+
+        self.init_data()
+
+    # 测试接口
+    def ceshi_api(self):
+        print(sys._getframe().f_code.co_name)
+        # self.ui.lb_sn.setText(recv_data.arryData[0])
+
+    # 打开升级文件
+    def openBinFile(self):
+        print(sys._getframe().f_code.co_name)
+        upgrade.FILE_NAME = ""
+        # 参数 _ 用于分割第二个返回值，防止 upgrade.FILE_NAME 生成数组
+        upgrade.FILE_NAME, _ = QtWidgets.QFileDialog.getOpenFileName(
+            self, "选择文件", "", "BIN(*.bin);;All Files(*)")
+        self.ui.lb_binpath.setText(upgrade.FILE_NAME)
+
+        upgrade.FILE_SZ = os.path.getsize(upgrade.FILE_NAME)
+        print(upgrade.FILE_SZ)
+        self.ui.lb_binsize.setText("%d" % upgrade.FILE_SZ+" bytes")
+
+        # 使用open函数打开文件，打开模式选择二进制读取'rb'
+        upgrade.FILE_FD = open(upgrade.FILE_NAME, 'rb')
+
+        # upgrade.FILE_FD.seek(0)
+        # print(upgrade.FILE_FD.read(4))
+        # for i in upgrade.FILE_FD:
+        #     print(i)
+        # upgrade.FILE_FD.close()
+
+    # 开始升级
+    def startUpgrade(self):
+        # update_loop.start()
+        global update_event, update_queue_start_cb, update_queue_send_cb
+        print(sys._getframe().f_code.co_name)
+        global devHandle
+        bytes = 6
+
+        # '//'表示向下取整除法
+        upgrade.FILE_CNT = upgrade.FILE_SZ//bytes
+
+        if upgrade.FILE_SZ % bytes > 0:
+            upgrade.FILE_CNT += 1
+
+        # 都是int型
+        # print(type(upgrade.FILE_SZ), type(upgrade.FILE_CNT))
+
+        psend_data = CAN_DataFrame(nSendType=0, bRemoteFlag=0,
+                                   bExternFlag=0, nDataLen=8, uID= 0x730)
+
+        # 固件大小
+        for i in range(0, 4):
+            psend_data.arryData[i] = (upgrade.FILE_SZ >> (8*i)) & 0xFF
+
+        # 总包数
+        for i in range(0, 2):
+            psend_data.arryData[i+4] = (upgrade.FILE_CNT >> (8*i)) & 0xFF
+
+        # CRC16校验
+
+        # 打印报文
+        # for i in range(0, 8):
+        #     print( hex(psend_data.arryData[i]), end='\t')
+
+        res = pDll.CAN_ChannelSend(devHandle, 0, pointer(psend_data), 1)
+        if res != CAN_RESULT_ERROR:
+            print("成功")
+        else:
+            print("失败")
+
+        data_res = update_queue_start_cb.get()
+        # for i in data_res:
+        #     print(hex(i))
+
+        if data_res[0] == 0:
+            print("允许升级")
+            for i in range(0, upgrade.FILE_CNT):
+                upgrade.FILE_FD.seek(bytes*upgrade.FILE_CNT)
+                print(upgrade.FILE_FD.read(bytes))
+
+        else:
+            print("禁止升级")
+
+        psend_data.uID= 0x732
+
+        for index in range(0, upgrade.FILE_CNT):
+            upgrade.FILE_FD.seek(bytes*index)
+            print(index, bytes*index, end='\t')
+            # for i in upgrade.FILE_FD.read(bytes):
+            #     print(hex(i), end='\t')
+
+            # 包索引
+            for i in range(0, 2):
+                psend_data.arryData[i] = (index >> (8*i)) & 0xFF
+
+            # 数据
+            tmp = 2
+            for i in upgrade.FILE_FD.read(bytes):
+                psend_data.arryData[tmp] = i
+                tmp += 1
+                # print(hex(upgrade.FILE_FD.read(bytes)[i]), end='\t')
+
+            # for i in psend_data.arryData:
+            #     print(hex(i), end='\t')
+
+            res = pDll.CAN_ChannelSend(devHandle, 0, pointer(psend_data), 1)
+            if res != CAN_RESULT_ERROR:
+                # print("升级包发送成功")
+
+                # 等待设备回复
+                data_res = update_queue_send_cb.get(timeout = 5)
+                if data_res[0] == 0:
+                    print("接收成功: ",index/upgrade.FILE_CNT*100)
+                    self.ui.progressBar.setValue(index/upgrade.FILE_CNT*100)
+                else:
+                    print("接收失败")
+                    for i in data_res:
+                        print(hex(i), end='\t')
+                    break
+            else:
+                print("升级包发送失败")
+                break
+
+        print("升级结束")
 
 
     # 接收线程
@@ -196,7 +296,7 @@ class MyWidget(QtWidgets.QWidget):
             test.STATE_VMN= recv_data2.arryData[1]&0xF0 >> 4
             test.STATE_PN = recv_data2.arryData[1]&0x0F
 
-            self.ui.lb_state.setText(str(test.STATE_MN)+'\t'+str(test.STATE_CPUN)+'\t'+str(test.STATE_VMN)+'\t'+str(test.STATE_PN))
+            self.ui.lb_state.setText("M卡数量："+str(test.STATE_MN)+'\t'+"CPU卡数量："+str(test.STATE_CPUN)+'\t'+"虚拟卡数量："+str(test.STATE_VMN)+'\t'+"唤醒引脚："+str(test.STATE_PN))
             # print(test.STATE_MN, test.STATE_CPUN, test.STATE_VMN, test.STATE_PN)
 
         elif recv_data2.uID == 0x705:
@@ -375,6 +475,7 @@ class MyWidget(QtWidgets.QWidget):
 if __name__ == "__main__":
     app = QtWidgets.QApplication([])
     recv_loop = recv_worker()
+    update_loop = update_worker()
     widget = MyWidget()
     widget.show()
     sys.exit(app.exec())
